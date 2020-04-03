6965458881721629c0f6c35f696892b1fe8fab81
ChenxiLiLi <416595479@qq.com>
2020-04-04 01:06:35 +0800

    修复Github登录

--- src/main/java/com/chenxi/community/provider/GithubProvider.java
+++ src/main/java/com/chenxi/community/provider/GithubProvider.java
@@ -39,7 +39,8 @@ public class GithubProvider {
     public GithubUser getUser(String accessToken) {
         OkHttpClient client = new OkHttpClient();
         Request request = new Request.Builder()
-                .url("https://api.github.com/user?access_token=" + accessToken)
+                .url("https://api.github.com/user")
+                .header("Authorization", "token " + accessToken)
                 .build();
         Response response = null;
         try {
